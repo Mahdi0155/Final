9a05bc1a5b92e94a8dd185ffa969d014b15c22ef
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-30 16:41:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ def main():
 
     application.add_handler(conv_handler)
 
-    WEBHOOK_URL = 'https://ooooo-fiwm.onrender.com/'  # آدرس ربات در Render
+    WEBHOOK_URL = 'https://final-4oxs.onrender.com'  # آدرس ربات در Render
 
     application.run_webhook(
         listen="0.0.0.0",
